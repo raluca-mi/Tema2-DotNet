dfe9987ea174204280039a3aff47ee41f909a4e2
raluca.mi <raluca.ioana169@gmail.com>
2023-04-23 18:40:36 +0300

    Created database from branch 7

---
+++ LaboratorAPI-lab-7/DataLayer/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                    .UseSqlServer("Server =.; Database = Tema2; Trusted_Connection = True; TrustServerCertificate = True")
+                    .LogTo(Console.WriteLine);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Class>().Property(e => e.Name).HasMaxLength(10);
+        }
+
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ LaboratorAPI-lab-7/Project/Program.cs
@@ -0,0 +1,32 @@
+using Project.Settings;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+Dependencies.Inject(builder);
+
+var app = builder.Build();
+
+/*app.UseSwagger(c =>
+{
+    c.RouteTemplate = "api/swagger/{documentName}/swagger.json";
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "Project");
+    c.RoutePrefix = "api/swagger";
+});*/
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
